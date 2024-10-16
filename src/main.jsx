0750669874ae86d48cd745f55dbf59dad866e405
aleanrocha <aleanrocha.dev@gmail.com>
2024-10-16 11:00:19 -0300

    add global styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { GlobalStyles } from './styles/GlobalStyles'
+
 import { Login } from './containers/Login'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <GlobalStyles />
     <Login />
   </StrictMode>
 )
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+  button {
+    cursor: pointer;
+    transition: 0.3s;
+  }
+  a {
+    text-decoration: none;
+    transition: 0.3s;
+  }
+  img {
+    width: 100%;
+  }
+`
