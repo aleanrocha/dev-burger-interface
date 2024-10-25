547c5039255644d61a4ec8979d1d3090f604499b
aleanrocha <aleanrocha.dev@gmail.com>
2024-10-25 07:11:38 -0300

    add load and display products and categories on menu screen

--- src/containers/Menu/index.jsx
+++ src/containers/Menu/index.jsx
@@ -1,6 +1,36 @@
-import { Container, Banner, CategoryMenu, ProductsContainer } from './styles'
+import { useState, useEffect } from 'react'
+import { api } from '../../services/api'
+
+import {
+  Container,
+  Banner,
+  CategoryMenu,
+  ProductsContainer,
+  CategoryButton,
+} from './styles'
+import { CardProduct } from '../../components/CardProduct'
+import { formatCurrency } from '../../utils/formatCurrency'
 
 export const Menu = () => {
+  const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const { data } = await api.get('/products')
+      const newProducts = data.map((product) => ({
+        formatedPrice: formatCurrency(product.price),
+        ...product,
+      }))
+      setProducts(newProducts)
+    }
+    const loadCategories = async () => {
+      const { data } = await api.get('/categories')
+      setCategories(data)
+    }
+    loadProducts()
+    loadCategories()
+  }, [])
   return (
     <Container>
       <Banner>
@@ -12,8 +42,21 @@ export const Menu = () => {
           <p>Esse cardápio está irresistível!</p>
         </div>
       </Banner>
-      <CategoryMenu></CategoryMenu>
-      <ProductsContainer></ProductsContainer>
+      <ProductsContainer>
+        <h2>Cardápio</h2>
+        <CategoryMenu>
+          {categories &&
+            categories.map((category) => (
+              <CategoryButton key={category.id}>{category.name}</CategoryButton>
+            ))}
+        </CategoryMenu>
+        <div>
+          {products &&
+            products.map((product) => (
+              <CardProduct key={product.id} product={product} />
+            ))}
+        </div>
+      </ProductsContainer>
     </Container>
   )
 }
--- src/containers/Menu/styles.js
+++ src/containers/Menu/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 import bannerMenu from '../../assets/banner-menu.png'
 import background from '../../assets/bg-login-2.png'
+import { Link } from 'react-router-dom'
 
 export const Container = styled.section`
   background: #252525 url('${background}');
@@ -47,6 +48,62 @@ export const Banner = styled.div`
     }
   }
 `
-export const CategoryMenu = styled.div``
+export const CategoryMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin: 2rem 1rem 0;
+  overflow-x: auto;
+  padding: 0.5rem 0;
+  @media screen and (max-width: 580px) {
+    justify-content: flex-start;
+    background-color: rgba(255, 255, 255, 0.3);
+    padding: 1rem;
+    border-radius: 5rem;
+  }
+`
+export const CategoryButton = styled(Link)`
+  color: #fff;
+  font-weight: 600;
+  text-transform: uppercase;
+  &:hover {
+    color: #9750a6;
+  }
+`
 
-export const ProductsContainer = styled.div``
+export const ProductsContainer = styled.div`
+  & > h2 {
+    color: #61a120;
+    text-align: center;
+    font-size: 2rem;
+    font-weight: 800;
+    text-transform: uppercase;
+    padding: 3rem 0 0.5rem;
+    margin-bottom: 1rem;
+    position: relative;
+    &::after {
+      content: '';
+      width: 60px;
+      height: 5px;
+      background-color: #61a120;
+      position: absolute;
+      left: 50%;
+      bottom: 0;
+      transform: translateX(-50%);
+    }
+  }
+  & > div:nth-of-type(2) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 4rem;
+    width: 90%;
+    max-width: 1280px;
+    margin: 0 auto;
+    padding: 4rem 0;
+    > div {
+      width: 250px;
+    }
+  }
+`
